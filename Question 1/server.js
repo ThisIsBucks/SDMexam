cb1c9471da967fdaf2783d3bcb652be65e5edf7c
ThisIsBucks <akshaypshinde9890@gmail.com>
2022-12-30 00:02:32 +0530

    hi

--- Question 1/server.js
+++ Question 1/server.js
@@ -2,7 +2,7 @@
 
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 8081
 
 app.get('/', (req, res) => {
   res.sendFile("/public/index.html",{root:__dirname});
